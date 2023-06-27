c1e064e216c14dea1f2575b655590253157fa973
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-27 22:51:10 +0300

    Create RegisterForm.jsx

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,20 @@
+const RegisterForm = () => {
+  return (
+    <form autoComplete="off">
+      <label>
+        Username
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+export default RegisterForm;
