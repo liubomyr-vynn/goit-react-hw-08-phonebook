c1605cb539b1e971b84a8a83eea01b3db8d74c0d
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-27 22:51:15 +0300

    Create LoginForm.jsx

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,16 @@
+const LoginForm = () => {
+  return (
+    <form autoComplete="off">
+      <label>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+export default LoginForm;
