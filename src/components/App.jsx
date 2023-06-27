956d0706b690e6b6fe4b265bf968328e54ae1b20
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-27 22:51:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
+// import ContactForm from './ContactForm/ContactForm';
+// import ContactList from './ContactList/ContactList';
+// import Filter from './Filter/Filter';
 
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchContacts } from '../redux/operations';
+import { Route, Routes, NavLink } from 'react-router-dom';
+// import styled from 'styled-components';
+import Home from 'pages/Home';
+import Phonebook from 'pages/Phonebook';
+import Register from 'pages/Register';
+import Login from 'pages/Login';
+
+// const StyledLink = styled(NavLink)`
+//   color: black;
+
+//   &.active {
+//     color: orange;
+//   }
+// `;
 
 const App = () => {
   const dispatch = useDispatch();
@@ -15,9 +29,25 @@ const App = () => {
 
   return (
     <div className="app">
-      <ContactForm />
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/phonebook">Phonebook</NavLink>
+        <NavLink to="/register">Register</NavLink>
+        <NavLink to="/login">Login</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/phonebook" element={<Phonebook />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      {/* <ContactForm />
       <Filter />
-      <ContactList />
+      <ContactList /> */}
     </div>
   );
 };
