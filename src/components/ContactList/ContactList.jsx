bd526a7f3dbec635301ce78172e5d86f1d50fc61
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-01 19:23:01 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteContact } from '../../redux/operations';
+import { deleteContact } from '../../redux/contacts/operations';
 
 const ContactList = () => {
   const contacts = useSelector(state => {
