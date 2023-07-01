095bf91923326629f3a535190e0ba434adf18143
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-01 19:10:47 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,11 @@
-const styles = {
-  container: {
-    minHeight: 'calc(100vh - 50px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 48,
-    textAlign: 'center',
-  },
-};
+import { Container, Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>
-        Phonebook welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-      </h1>
-    </div>
+    <Container>
+      <Typography variant="h3" component="div">
+        Phonebook welcome page
+      </Typography>
+    </Container>
   );
 }
