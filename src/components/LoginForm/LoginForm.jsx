5c8081564f3b4691cef620692c261901e85c4cdb
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-01 19:13:09 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,16 +1,58 @@
+import { Box, Button, TextField } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { logIn } from '../../redux/auth/operations';
+
 const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+
+    const data = new FormData(e.currentTarget);
+
+    const user = {
+      email: data.get('email'),
+      password: data.get('password'),
+    };
+    console.log(user);
+    dispatch(logIn(user));
+    form.reset();
+  };
+
   return (
-    <form autoComplete="off">
-      <label>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <TextField
+        id="email"
+        label="Email"
+        variant="outlined"
+        type="email"
+        name="email"
+      />
+
+      <TextField
+        id="password"
+        label="Password"
+        variant="outlined"
+        type="password"
+        name="password"
+      />
+
+      <Button variant="contained" type="submit">
+        Log In
+      </Button>
+    </Box>
   );
 };
 export default LoginForm;
