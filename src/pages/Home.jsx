39d8526b34329e5d3057284942a3864edb96072e
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-02 10:43:21 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import { Container, Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <Container>
+    <Container
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
       <Typography variant="h3" component="div">
         Phonebook welcome page
       </Typography>
