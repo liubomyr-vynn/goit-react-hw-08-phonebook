79b5c6472bd20c8a9e908cf9209b35062204e62a
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-03 11:09:43 +0300

    Update Phonebook.jsx

--- src/pages/Phonebook.jsx
+++ src/pages/Phonebook.jsx
@@ -1,14 +1,16 @@
+import React from 'react';
+import { Box } from '@mui/material';
 import ContactForm from '../components/ContactForm/ContactForm';
 import ContactList from '../components/ContactList/ContactList';
 import Filter from '../components/Filter/Filter';
 
 const Phonebook = () => {
   return (
-    <div>
+    <Box sx={{ maxWidth: '320px', margin: '0 auto' }}>
       <ContactForm />
       <Filter />
       <ContactList />
-    </div>
+    </Box>
   );
 };
 
