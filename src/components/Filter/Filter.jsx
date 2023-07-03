de813d172e1c82512f53dc63c55448220e54dde3
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-03 11:08:52 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setFilterValue } from '../../redux/contacts/operations';
+import { Typography, TextField, Box } from '@mui/material';
 
 const Filter = () => {
   const filterValue = useSelector(state => state.contacts.filter);
@@ -12,17 +13,33 @@ const Filter = () => {
   };
 
   return (
-    <div className="section">
-      <h2 className="section__title">Contacts</h2>
-      <label className="filter__label">Find contacts by name</label>
-      <input
-        className="form__input"
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Typography
+        variant="h5"
+        component="h2"
+        align="center"
+        sx={{ marginTop: '35px' }}
+        gutterBottom
+      >
+        Contacts
+      </Typography>
+      <label>Find contacts by name</label>
+      <TextField
         type="text"
         value={filterValue}
         onChange={handleChange}
         name="filter"
+        size="small"
+        variant="outlined"
+        sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
       />
-    </div>
+    </Box>
   );
 };
 
